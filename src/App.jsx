7421ad7a71ea04c11942b4f3f8dd235f18f7c00a
josalero <3307982+josalero@users.noreply.github.com>
2024-12-04 10:54:04 -0600

    More progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import { Box, CssBaseline } from '@mui/material';
